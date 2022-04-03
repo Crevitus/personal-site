82dada0c17ae3655e531efbc17005dfb832f68a7
Crevitus <iam@bencrevis.uk>
2022-04-03 17:09:18 +0100

    Fix production build

--- remix.config.js
+++ remix.config.js
@@ -1,12 +1,17 @@
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
-module.exports = {
-  // serverBuildTarget: 'netlify',
-  // server: './server.js',
-  ignoredRouteFiles: ['.*'],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: ".netlify/functions-internal/server.js",
-  // publicPath: "/build/",
-};
+
+module.exports =
+  process.env.NODE_ENV === 'production'
+    ? {
+        serverBuildTarget: 'netlify',
+        server: './server.js',
+        ignoredRouteFiles: ['.*'],
+        appDirectory: 'app',
+        assetsBuildDirectory: 'public/build',
+        publicPath: '/build/',
+      }
+    : {
+        ignoredRouteFiles: ['.*'],
+      };
