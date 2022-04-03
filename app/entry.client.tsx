d83376faf041c6a529861059d2655deb9b3838c0
Crevitus <iam@bencrevis.uk>
2022-04-03 17:34:58 +0100

    Try old hydration

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,4 @@
+import { hydrate } from 'react-dom';
 import { RemixBrowser } from 'remix';
-import { hydrateRoot } from 'react-dom/client';
 
-hydrateRoot(document, <RemixBrowser />);
+hydrate(<RemixBrowser />, document);
